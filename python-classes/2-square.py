3f0b866ee0ceb93a5025500e9d2da01c8992f657
Elias301010 <elias301010@gmail.com>
2025-07-17 09:38:32 +0400

    xtcyvubijnkl

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,2 +1,15 @@
 #!/usr/bin/python3
-"""Define a Square class"""
+class Square:
+    """Defines a square."""
+
+    def __init__(self, size=0):
+        """Initialize a new square with optional size."""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size  # private instance attribute
+
+    def area(self):
+        """Return the area of the square."""
+        return self.__size * self.__size
