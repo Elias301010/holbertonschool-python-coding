d477a6e9d8a4921d1f882ff1ab3f176c44c6c29b
Elias301010 <elias301010@gmail.com>
2025-07-17 09:42:15 +0400

    zewxrgvybhjnkml,

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""This module defines a Square class for representing squares."""
+
 class Square:
     """Defines a square."""
 
@@ -8,7 +10,7 @@ class Square:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size  # private instance attribute
+        self.__size = size
 
     def area(self):
         """Return the area of the square."""
